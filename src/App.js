f289170c1ae0b67e5823d3dafc1ae6bf978975af
kaspqr <kasparvella@gmail.com>
2023-08-31 18:11:24 +0300

    remove hover styles for mobiles

--- src/App.js
+++ src/App.js
@@ -57,6 +57,29 @@ import FAQ from "./features/policy/Faq"
 
 function App() {
 
+  function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+      || navigator.maxTouchPoints > 0
+      || navigator.msMaxTouchPoints > 0
+  }
+  
+  if (hasTouch()) { // remove all the :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+      for (let si in document.styleSheets) {
+        let styleSheet = document.styleSheets[si]
+        if (!styleSheet.rules) continue
+
+        for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue
+
+          if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri)
+          }
+        }
+      } 
+    } catch (ex) {}
+  }
+
   useTitle('Paw Retriever')
 
   return (
