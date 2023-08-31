8a0dbd5da6aa56713686fd07fe2e843cbeb6ea02
kaspqr <kasparvella@gmail.com>
2023-08-31 18:21:41 +0300

    let to var in hasTouch

--- src/App.js
+++ src/App.js
@@ -65,15 +65,15 @@ function App() {
   
   if (hasTouch()) { // remove all the :hover stylesheets
     try { // prevent exception on browsers not supporting DOM styleSheets properly
-      for (let si in document.styleSheets) {
-        let styleSheet = document.styleSheets[si]
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si]
         if (!styleSheet.rules) continue
 
-        for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
           if (!styleSheet.rules[ri].selectorText) continue
 
           if (styleSheet.rules[ri].selectorText.match(':hover')) {
-          styleSheet.deleteRule(ri)
+            styleSheet.deleteRule(ri)
           }
         }
       } 
