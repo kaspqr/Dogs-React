79a5dc9e1272a6a1ce521588af7fc227cbb2c89b
kaspqr <kasparvella@gmail.com>
2023-12-16 23:28:48 +0200

    changed back

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/auth-slices/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${process.env.BACKEND_URL}`,
+  baseUrl: 'https://pawretriever-api.onrender.com',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
