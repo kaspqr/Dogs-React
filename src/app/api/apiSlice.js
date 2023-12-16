4fb457ffea4e4b1c3a2105e1cb52eeb665cdcdfe
kaspqr <kasparvella@gmail.com>
2023-12-16 23:26:00 +0200

    template literal

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/auth-slices/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://pawretriever-api.onrender.com',
+  baseUrl: `${process.env.BACKEND_URL}`,
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
